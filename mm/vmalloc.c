441b169dbd17dd0505004cf3e2b2575cdcfa4a9e
Linus Torvalds <torvalds@linux-foundation.org>
2011-07-22 16:44:08 -0700

    Merge branch 'core-rcu-for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/tip/linux-2.6-tip
    * 'core-rcu-for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/tip/linux-2.6-tip:
      rcu: Fix wrong check in list_splice_init_rcu()
      net,rcu: Convert call_rcu(xt_rateest_free_rcu) to kfree_rcu()
      sysctl,rcu: Convert call_rcu(free_head) to kfree
      vmalloc,rcu: Convert call_rcu(rcu_free_vb) to kfree_rcu()
      vmalloc,rcu: Convert call_rcu(rcu_free_va) to kfree_rcu()
      ipc,rcu: Convert call_rcu(ipc_immediate_free) to kfree_rcu()
      ipc,rcu: Convert call_rcu(free_un) to kfree_rcu()
      security,rcu: Convert call_rcu(sel_netport_free) to kfree_rcu()
      security,rcu: Convert call_rcu(sel_netnode_free) to kfree_rcu()
      ia64,rcu: Convert call_rcu(sn_irq_info_free) to kfree_rcu()
      block,rcu: Convert call_rcu(disk_free_ptbl_rcu_cb) to kfree_rcu()
      scsi,rcu: Convert call_rcu(fc_rport_free_rcu) to kfree_rcu()
      audit_tree,rcu: Convert call_rcu(__put_tree) to kfree_rcu()
      security,rcu: Convert call_rcu(whitelist_item_free) to kfree_rcu()
      md,rcu: Convert call_rcu(free_conf) to kfree_rcu()
    (cherry picked from commit 0342cbcfced2ee937d7c8e1c63f3d3082da7c7dc)

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -492,13 +492,6 @@ overflow:
 	return ERR_PTR(-EBUSY);
 }
 
-static void rcu_free_va(struct rcu_head *head)
-{
-	struct vmap_area *va = container_of(head, struct vmap_area, rcu_head);
-
-	kfree(va);
-}
-
 static void __free_vmap_area(struct vmap_area *va)
 {
 	BUG_ON(RB_EMPTY_NODE(&va->rb_node));
@@ -531,7 +524,7 @@ static void __free_vmap_area(struct vmap_area *va)
 	if (va->va_end > VMALLOC_START && va->va_end <= VMALLOC_END)
 		vmap_area_pcpu_hole = max(vmap_area_pcpu_hole, va->va_end);
 
-	call_rcu(&va->rcu_head, rcu_free_va);
+	kfree_rcu(va, rcu_head);
 }
 
 /*
@@ -878,13 +871,6 @@ static struct vmap_block *new_vmap_block(gfp_t gfp_mask)
 	return vb;
 }
 
-static void rcu_free_vb(struct rcu_head *head)
-{
-	struct vmap_block *vb = container_of(head, struct vmap_block, rcu_head);
-
-	kfree(vb);
-}
-
 static void free_vmap_block(struct vmap_block *vb)
 {
 	struct vmap_block *tmp;
@@ -897,7 +883,7 @@ static void free_vmap_block(struct vmap_block *vb)
 	BUG_ON(tmp != vb);
 
 	free_vmap_area_noflush(vb->va);
-	call_rcu(&vb->rcu_head, rcu_free_vb);
+	kfree_rcu(vb, rcu_head);
 }
 
 static void purge_fragmented_blocks(int cpu)
