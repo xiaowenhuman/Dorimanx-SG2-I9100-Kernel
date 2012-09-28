3c45026d045fc4289e75747071cc8981cc234e81
Renaud Allard <renaud@allard.it>
2012-09-28 16:26:06 +0200

    bcmdhd: get back to 3.4 driver

--- drivers/net/wireless/bcmdhd/include/bcm_cfg.h
+++ drivers/net/wireless/bcmdhd/include/bcm_cfg.h
@@ -1,4 +1,6 @@
 /*
+ * BCM common config options
+ *
  * Copyright (C) 1999-2012, Broadcom Corporation
  * 
  *      Unless you and Broadcom execute a separate written software license
@@ -19,30 +21,9 @@
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
  *
- * Fundamental types and constants relating to 802.1D
- *
- * $Id: 802.1d.h 241182 2011-02-17 21:50:03Z $
+ * $Id: bcm_cfg.h 294399 2011-11-07 03:31:22Z $
  */
 
-#ifndef _802_1_D_
-#define _802_1_D_
-
-
-#define	PRIO_8021D_NONE		2	
-#define	PRIO_8021D_BK		1	
-#define	PRIO_8021D_BE		0	
-#define	PRIO_8021D_EE		3	
-#define	PRIO_8021D_CL		4	
-#define	PRIO_8021D_VI		5	
-#define	PRIO_8021D_VO		6	
-#define	PRIO_8021D_NC		7	
-#define	MAXPRIO			7	
-#define NUMPRIO			(MAXPRIO + 1)
-
-#define ALLPRIO		-1	
-
-
-#define PRIO2PREC(prio) \
-	(((prio) == PRIO_8021D_NONE || (prio) == PRIO_8021D_BE) ? ((prio^2)) : (prio))
-
+#ifndef _bcm_cfg_h_
+#define _bcm_cfg_h_
 #endif 
