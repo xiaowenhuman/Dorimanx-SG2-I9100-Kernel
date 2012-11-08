2d16899850a65e45a7d4aa0d62aa79c0affe55e3
Yuri.Sh <yuri@bynet.co.il>
2012-11-08 12:00:35 +0200

    Replaced EXtweaks with NEW STweaks by GM, extweaks not supported now!

--- compress-payload.sh
+++ compress-payload.sh
@@ -2,14 +2,14 @@
 cd PAYLOAD
 chmod 644 Superuser.apk
 chmod 755 su
-chmod 644 Extweaks.apk
+chmod 644 STweaks.apk
 xz -zekv9 Superuser.apk
 xz -zekv9 su
-xz -zekv9 Extweaks.apk
+xz -zekv9 STweaks.apk
 
 mv Superuser.apk.xz res/misc/payload/
 mv su.xz res/misc/payload/
-mv Extweaks.apk.xz res/misc/payload/
+mv STweaks.apk.xz res/misc/payload/
 
 rm -f ../payload.tar
 tar -cv res > payload.tar
